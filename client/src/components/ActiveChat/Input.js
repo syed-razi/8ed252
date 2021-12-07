caea6dd2db130efd8b3e9eb88e367f7d2cb3ffa9
Razi <syedru@mcmaster.ca>
2021-12-07 13:05:15 -0500

    Add new messages immediately to chat
    Resolved this by dispatching fetchConversations immediately after
    postMessage in the input component. This updates the state with the
    latest conversations, thus causing ActiveChat to re-render as it
    subscribes to the conversations state.

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FormControl, FilledInput } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
-import { postMessage } from "../../store/utils/thunkCreators";
+import { fetchConversations, postMessage } from "../../store/utils/thunkCreators";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -20,7 +20,7 @@ const useStyles = makeStyles(() => ({
 const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
-  const { postMessage, otherUser, conversationId, user } = props;
+  const { fetchConversations, postMessage, otherUser, conversationId, user } = props;
 
   const handleChange = (event) => {
     setText(event.target.value);
@@ -36,6 +36,7 @@ const Input = (props) => {
       sender: conversationId ? null : user
     };
     await postMessage(reqBody);
+    fetchConversations();
     setText("");
   };
 
@@ -60,6 +61,9 @@ const mapDispatchToProps = (dispatch) => {
     postMessage: (message) => {
       dispatch(postMessage(message));
     },
+    fetchConversations: () => {
+      dispatch(fetchConversations());
+    }
   };
 };
 
