399080d3384ffd4be67be135d82e3a6d04453577
Razi <syedru@mcmaster.ca>
2021-12-09 15:19:51 -0500

    Revert db changes and order messages on client
    I reverted the db change as it reversed how conversations were
    ordered as well. I instead simply re-ordered messages on the client end.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,15 +8,22 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
+      {messages
+        .sort((a, b) => a.id - b.id)
+        .map((message) => {
+          const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-      })}
+          return message.senderId === userId ? (
+            <SenderBubble key={message.id} text={message.text} time={time} />
+          ) : (
+            <OtherUserBubble
+              key={message.id}
+              text={message.text}
+              time={time}
+              otherUser={otherUser}
+            />
+          );
+        })}
     </Box>
   );
 };
