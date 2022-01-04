0877d77aa436f3379468d82195f08ffcf0b39e4d
Razi <syedru@mcmaster.ca>
2022-01-04 02:07:42 -0500

    Set read according to active conversation
    Since active conversations of users are tracked now, it's more
    appropriate to set read according to that rather than the default of
    false.

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -9,11 +9,11 @@ router.post("/", async (req, res, next) => {
       return res.sendStatus(401);
     }
     const senderId = req.user.id;
-    const { recipientId, text, conversationId, sender } = req.body;
+    const { recipientId, text, conversationId, sender, read } = req.body;
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId, read: false });
+      const message = await Message.create({ senderId, text, conversationId, read });
       return res.json({ message, sender });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
@@ -40,7 +40,7 @@ router.post("/", async (req, res, next) => {
       senderId,
       text,
       conversationId: conversation.id,
-      read: false,
+      read,
     });
     res.json({ message, sender });
   } catch (error) {
