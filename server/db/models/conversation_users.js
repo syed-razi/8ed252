f35f484c4d4b2e4f96aee258e42ff0958af4aadd
Razi <syedru@mcmaster.ca>
2022-01-10 10:58:59 -0500

    Change findConversation to work with many users
    Originally findConversation only received a sender and a receiver as
    arguements. However with conversations of more than 2 users, the
    function did not work. The changes allow for a conversation to now be
    found given any number of users.

--- server/db/models/conversation_users.js
+++ server/db/models/conversation_users.js
@@ -20,4 +20,22 @@ const ConversationUsers = db.define("conversation_users", {
   },
 });
 
+// find conversation given the sender and recipients
+
+ConversationUsers.findConversation = async function (senderId, recipientIds) {
+  const userIds = [senderId, ...recipientIds];
+  const numUserIds = userIds.length;
+
+  const conversation = await ConversationUsers.findAll({
+    attributes: ["conversationId"],
+    group: ["conversationId"],
+    having: 
+      Sequelize.literal(
+        `SUM(CASE WHEN "userId" in (${userIds}) then 1 else 0 end) = ${numUserIds} AND count(*) = ${numUserIds}`
+      ),
+  });
+
+  return conversation;
+};
+
 module.exports = ConversationUsers;
